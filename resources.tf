8f84c555c5f192ecb5258aa04288dfbe0ef14304
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-09-20 20:03:27 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -23,7 +23,4 @@ resource "aws_instance" "testInstance" {
   vpc_security_group_ids = [module.networkModule.sg_22_id]
   key_name = aws_key_pair.ec2key.key_name
 
-  tags {
-		Environment = var.environment_tag
-	}
 }
